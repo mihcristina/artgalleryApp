848466e41d0f800db18362b3b4d13ee14bc0ede1
Michelli Cristina <contato@michellicristina.com.br>
2025-05-26 15:50:03 -0300

    feat: - commit inicial

---
+++ ArtGalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtGalleryApp
+//
+//  Created by Michelli Cristina de Paulo Lima on 26/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
